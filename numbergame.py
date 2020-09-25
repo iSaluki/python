fd21a4917c947e612660e38c2b5b511eb2ac142d
iSaluki <37915796+iSaluki@users.noreply.github.com>
2020-09-25 11:17:01 +0100

    Update numbergame.py
    Added a basic leaderboard (stored in RAM)
    Added a play again menu
    Fixed some bugs

--- numbergame.py
+++ numbergame.py
@@ -2,15 +2,33 @@ import random
 number = random.randint(1,100)
 # print (number) 
 # Enable the above line by removing the '#'. It will print the random number for debug purposes.
-attemptsLeft = 3
-while attemptsLeft !=0:
+totalAttempts = 0
+hasWon = False
+highscores = []
+newGame = "y"
+
+def Win():
+	global totalAttempts, newGame
+	print ("It took you", totalAttempts, "guesses to get the number")
+	name = input("What is your name? ")
+	highscores.append([name, totalAttempts])
+	print (highscores)
+	totalAttempts = 0
+	newGame = input("Would you like to play again?[y/n] ")
+	newGame = newGame[0].lower()
+
+
+while  newGame == "y":
 	guess = input("What do you think the number is? ")
 	while not guess.isdigit():
 		guess = input("Not a number. Please enter a 	number.")
 	guess = int(guess)
 	if guess == number:
 		print ("You win!")
-
+		hasWon == True
+		newGame == "n"
+		number = random.randint(1,100)
+		Win()
 	else:
 		print ("Wrong!")
 		if guess > number:
@@ -18,4 +36,6 @@ while attemptsLeft !=0:
 		elif guess < number:
 			guessIs = "too low"
 		print (guess, "is", guessIs)
-		attemptsLeft -= 1
+		totalAttempts += 1
+#if newGame != "y":
+print ("Goodbye!")
