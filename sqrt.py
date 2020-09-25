2ffc5c4dc7b87dcc82e59e00188bfb28c9e75d05
iSaluki <37915796+iSaluki@users.noreply.github.com>
2020-09-25 17:02:52 +0100

    Update sqrt.py
    Added a square number function

--- sqrt.py
+++ sqrt.py
@@ -1,12 +1,36 @@
 import math
 
-while True:
-	msg ="Enter number to square root: "
+msg_sqrt ="Enter number to square root: "
+msg_init = "Choose function:"
+msg_sqr = "Enter number to square:"
 
-	num = input(msg)
+def Sqrt():		
+	num = input(msg_sqrt)
 	while not num.isdigit():
-		num = input(msg)
+		num = input(msg_sqrt)
 	
-	num = int(num)
+	num = float(num)
 	sqrt = (math.sqrt(num))
 	print ("Square root:", sqrt)
+	
+def Sqr():
+	num = input(msg_sqr)
+	while not num.isdigit():
+		num = input(msg_sqr)
+	sqr = float(num)**2
+	print("Squared number:", sqr)
+
+while True:
+	print ("--------")
+	print ("Functions:")
+	print("[1] Square root")
+	print ("[2] Square")
+	function = input("Select function:")
+	
+	if function == "1":
+		Sqrt()
+	elif function == "2":
+		Sqr()
+	else:
+		function = input("Select function:")
+		
