609737dfaace73a59acc41d6514bb281a0855f37
iSaluki <37915796+iSaluki@users.noreply.github.com>
2020-09-26 10:30:03 +0100

    Update poly_perim.py
    Made it use classes properly and improved the code overall.

--- code/utility/poly_perim.py
+++ code/utility/poly_perim.py
@@ -2,6 +2,10 @@ class Shape:
 	def __init__ (self,sides,length):
 		self.sides = sides
 		self.length = length
+	def perimeter(self):
+		return self.sides * self.length
+		
+		
 inSides = input("Enter number of sides: ")
 while not inSides.isdigit():
 	inSides = input("Enter number of sides: ")
@@ -11,8 +15,7 @@ while not inLength.isdigit():
 	inLength = input("Enter length of sides: ")
 inLength = float(inLength)
 
-Shape.sides = inSides
-Shape.length = inLength
+polygon = Shape(inSides, inLength)
 
-output = Shape.length * Shape.sides
-print ("The perimeter is", output)
+
+print ("The perimeter is", polygon.perimeter())
