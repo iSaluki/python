850333f3f048014b5a621eecc06b6d20cee69a63
iSaluki <37915796+iSaluki@users.noreply.github.com>
2020-09-29 18:09:53 +0100

    Create google.py
    Open Chrome and make a Google search based on user input.

---
+++ code/automation/google.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+query = input("What would you like to search? ")
+
+
+driver = webdriver.Chrome()
+driver.get("https://google.com/")
+search_bar = driver.find_element_by_name("q")
+search_bar.send_keys(query)
+search_bar.send_keys(Keys.ENTER)
+search_bar.click()
