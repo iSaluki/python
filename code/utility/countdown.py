db7c42afb2695a3581347e986939ff6456cf2649
iSaluki <37915796+iSaluki@users.noreply.github.com>
2020-09-30 12:39:00 +0100

    Create countdown.py
    Simple countdown timer in seconds

---
+++ code/utility/countdown.py
@@ -0,0 +1,13 @@
+import time
+
+t = input("Enter start time: ")
+while not t.isdigit():
+    t = input("Enter start time: ")
+t = int(t)
+
+while t:
+    timer = t
+    time.sleep(1)
+    t -= 1
+    print (t)
+
